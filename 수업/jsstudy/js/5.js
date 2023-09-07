0bd8158cf60172eff601a8aa1fa963594897189e
wlalsplus100 <jiminelp@gmail.com>
2023-09-07 14:47:00 +0900

    Html learn

--- 수업/jsstudy/js/5.js
+++ 수업/jsstudy/js/5.js
@@ -61,3 +61,51 @@ console.log(num1 + num2);
 console.log(num1 - num2);
 console.log(num1 * num2);
 console.log(num1 / num2);
+
+console.clear();
+
+console.log(Number(true));
+console.log(Number("1900"));
+console.log(Number("15.23"));
+console.log(Number("Hi"));
+console.log(Number(null));
+console.log(Number("0000001470"));
+console.log(Number("0000001470.70"));
+console.log(parseInt(3400.123));
+console.log(parseFloat(3400.123));
+let num3 = 10;
+let isEmpty = true;
+let null001 = null;
+console.log(num3.toString());
+console.log(num3.toString(2));
+console.log(isEmpty.toString());
+// console.log(null001.toString());
+console.log(String(null001));
+console.log(String(undefined));
+console.log(String(num3));
+console.log(String("21.3"));
+
+console.log(Boolean(2 * 3));
+console.log(Boolean(2 * 0));
+console.log(Boolean("hi"));
+console.log(Boolean(""));
+
+console.clear();
+
+console.log(5 / 2);
+console.log(5 % 2);
+console.log(Math.floor(5 / 2));
+console.log(5 ** 2);
+console.log(Math.pow(5, 2));
+
+console.log("1" == 1);
+console.log("1" === 1);
+console.log("1" != 1);
+console.log("1" !== 1);
+
+console.clear();
+
+console.log(+false);
+console.log(+null);
+console.log(+"text");
+console.log(+undefined);
