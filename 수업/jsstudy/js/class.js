e63250c9655f0afe1985c36bee6d54f527b9391a
wlals <jiminelp@gmail.com>
2023-10-19 08:47:29 +0900

    📝:: JS learn

--- 수업/jsstudy/js/class.js
+++ 수업/jsstudy/js/class.js
@@ -53,3 +53,117 @@
 // apple.display();
 
 // ============================================================================================
+
+// class Car {
+//   static count = 0;
+//   color = "";
+//   speed = 0;
+//   constructor(speed) {
+//     this.speed = speed;
+//     Car.count += 1;
+//   }
+// }
+
+// const myCar1 = new Car(30);
+// console.log(
+//   `자동차1의 속도 : ${myCar1.speed} 총 생산된 자동차수 : ${Car.count}`
+// );
+// const myCar2 = new Car(50);
+// console.log(
+//   `자동차2의 속도 : ${myCar2.speed} 총 생산된 자동차수 : ${Car.count}`
+// );
+// const myCar3 = new Car(69);
+// console.log(
+//   `자동차3의 속도 : ${myCar3.speed} 총 생산된 자동차수 : ${Car.count}`
+// );
+// const myCar4 = new Car(89);
+// console.log(
+//   `자동차4의 속도 : ${myCar4.speed} 총 생산된 자동차수 : ${Car.count}`
+// );
+
+// ==================================
+
+// class Fruit {
+//   type = "과일";
+//   #name;
+//   #nameKo;
+//   constructor(name, nameKo) {
+//     this.#name = name;
+//     this.#nameKo = nameKo;
+//   }
+//   display = () => {
+//     console.log(`${this.#name} : ${this.#nameKo}`);
+//   };
+// }
+
+// const apple = new Fruit("apple", "사과");
+// console.log(apple);
+// apple.display();
+
+// ================================================================
+
+// class Student {
+//   constructor(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+//   get fullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+
+//   set fullName(value) {
+//     console.log("set", value);
+//   }
+// }
+
+// const student = new Student("길동", "홍");
+// student.firstName = "광수";
+// console.log(student.fullName);
+// student.fullName = "이순신";
+
+// ==============================================
+
+// class Student {
+//   #name;
+//   #age;
+//   constructor(name, age) {
+//     this.#name = name;
+//     this.#age = age;
+//   }
+
+//   get getAge() {
+//     return this.#age;
+//   }
+//   get getName() {
+//     return this.#name;
+//   }
+
+//   set setAge(age) {
+//     this.#age = age;
+//   }
+
+//   set setName(name) {
+//     this.#name = name;
+//   }
+// }
+
+// const student = new Student("박지민", 17);
+// console.log(`${student.getName} ${student.getAge}`);
+// student.setAge = 18;
+// student.setName = "민지박";
+// console.log(`${student.getName} ${student.getAge}`);
+
+// ===================================================
+
+class Counter {
+  #count;
+  constructor(count) {
+    this.#count = count;
+  }
+
+  get getCount() {
+    return this.#count;
+  }
+}
+
+const counter = new Counter();
